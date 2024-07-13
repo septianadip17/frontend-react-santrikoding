044c49f12506258d50e06f7ec61bd21427335ad0
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-13 10:34:16 +0700

    success show name

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,23 @@
 //import useContext
 import { useContext } from "react";
 
+//import context
 import { AuthContext } from "../context/AuthContext";
 
+//import react router dom
 import { Routes, Route, Navigate } from "react-router-dom";
 
+//import view home
 import Home from "../views/index.jsx";
 
+//import view register
 import Register from "../views/auth/register.jsx";
 
+//import view login
 import Login from "../views/auth/login.jsx";
+
+//import view admin dashboard
+import Dashboard from "../views/admin/dashboard/index.jsx";
 
 export default function AppRoutes() {
   //destructure context "isAuthenticated"
@@ -41,6 +49,14 @@ export default function AppRoutes() {
           ) : (
             <Login />
           )
+        }
+      />
+
+      {/* route "/admin/dashboard" */}
+      <Route
+        path="/admin/dashboard"
+        element={
+          isAuthenticated ? <Dashboard /> : <Navigate to="/login" replace />
         }
       />
     </Routes>
