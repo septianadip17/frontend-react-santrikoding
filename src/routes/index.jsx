420ddc05787abb6ae7bb4658ed7043f1feb099ad
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-13 08:50:35 +0700

    routes index.jsx deleted comments

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,19 +1,14 @@
 //import useContext
 import React, { useContext } from "react";
 
-//import context
 import { AuthContext } from "../context/AuthContext";
 
-//import react router dom
 import { Routes, Route, Navigate } from "react-router-dom";
 
-//import view home
 import Home from "../views/home/index.jsx";
 
-//import view register
 import Register from "../views/auth/register.jsx";
 
-//import view login
 import Login from "../views/auth/login.jsx";
 
 export default function AppRoutes() {
